696d402fb5356fe2a2206a93e5b413f6311838d8
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-03-23 20:14:49 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('App is running...')
 })
 
+app.get('/hellow', (req, res) => {
+    res.send('Hellow World')
+})
+
 app.get('/:scrip', (req, res) => {
     const { scrip } = req.params;
     const price = Math.floor(Math.random() * (500 - 400 + 1)) + 400; // Generate random price between 400 and 500
