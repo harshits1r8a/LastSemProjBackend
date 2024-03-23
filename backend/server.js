0b7bf42a9be48b247bda7251059d3356c6b00d30
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-03-23 19:16:11 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import 'dotenv/config'
+
+const port = process.env.PORT || 5555
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('App is running...')
+})
+
+app.get('/:scrip', (req, res) => {
+    const { scrip } = req.params;
+    const price = Math.floor(Math.random() * (500 - 400 + 1)) + 400; // Generate random price between 400 and 500
+    const data = [{ scrip, price }];
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`App listening in ${process.env.NODE_ENV} mode on port : ${port}`);
+})
